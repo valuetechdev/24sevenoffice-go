fc0c63e54b9019b6e5450622f1fdfa4c2e08e4b3
Tim Hårek Andreassen <tim@value.no>
2025-05-29 22:15:56 +0200

    Add rest24 API

---
+++ rest24/client_test.go
@@ -0,0 +1,29 @@
+package rest24
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientInitialization(t *testing.T) {
+	require := require.New(t)
+
+	c, err := New(&TokenOpts{
+		ClientId:       os.Getenv("TFSO_APP_ID"),
+		ClientSecret:   os.Getenv("TFSO_SECRET"),
+		OrganizationId: "363976382792241",
+	})
+	require.NoError(err)
+
+	res, err := c.GetMeWithResponse(context.Background(), nil)
+	require.NoError(err)
+	require.NotNil(res)
+	require.Equal(http.StatusOK, res.StatusCode())
+	require.NotEmpty(res.Body)
+	require.NotNil(res.JSON200)
+	require.Equal("NO", *res.JSON200.CountryCode)
+}
