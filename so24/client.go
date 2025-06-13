d0e08042d92b22bf84f09c7ace5488f4f83e6455
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2025-06-13 16:37:43 +0200

    feat: add "WithOptions" versions of the new funcs to allow for more custom setup

--- so24/client.go
+++ so24/client.go
@@ -57,8 +57,12 @@ type Credentials struct {
 	PayrollAPI    string
 }
 
-// panic if missing credentials
 func NewAuthenticatedClient(creds *Credentials) (*Client, error) {
+	return NewAuthenticatedClientWithOptions(creds)
+}
+
+// panic if missing credentials
+func NewAuthenticatedClientWithOptions(creds *Credentials, opts ...soap.Option) (*Client, error) {
 	if creds == nil {
 		return nil, fmt.Errorf("creds cannot be nil")
 	}
@@ -75,7 +79,8 @@ func NewAuthenticatedClient(creds *Credentials) (*Client, error) {
 	return c, nil
 }
 
-func NewClient(creds *Credentials) (*Client, error) {
+func NewClientWithOptions(creds *Credentials, opts ...soap.Option) (*Client, error) {
+
 	if creds == nil {
 		return nil, fmt.Errorf("creds cannot be nil")
 	}
@@ -84,34 +89,34 @@ func NewClient(creds *Credentials) (*Client, error) {
 	headers := make(map[string]string)
 
 	authService := auth24.NewAuthenticateSoap(
-		newSoapClient(auth_url, headers),
+		newSoapClient(auth_url, headers, opts...),
 	)
 	invoiceService := invoice24.NewInvoiceServiceSoap(
-		newSoapClient(invoice_url, headers),
+		newSoapClient(invoice_url, headers, opts...),
 	)
 	productService := product24.NewProductServiceSoap(
-		newSoapClient(product_url, headers),
+		newSoapClient(product_url, headers, opts...),
 	)
 	accountService := account24.NewAccountServiceSoap(
-		newSoapClient(account_url, headers),
+		newSoapClient(account_url, headers, opts...),
 	)
 	companyService := company24.NewCompanyServiceSoap(
-		newSoapClient(company_url, headers),
+		newSoapClient(company_url, headers, opts...),
 	)
 	clientService := client24.NewClientServiceSoap(
-		newSoapClient(client_url, headers),
+		newSoapClient(client_url, headers, opts...),
 	)
 	personService := person24.NewPersonServiceSoap(
-		newSoapClient(person_url, headers),
+		newSoapClient(person_url, headers, opts...),
 	)
 	projectService := project24.NewProjectServiceSoap(
-		newSoapClient(project_url, headers),
+		newSoapClient(project_url, headers, opts...),
 	)
 	transactionService := transaction24.NewTransactionServiceSoap(
-		newSoapClient(transaction_url, headers),
+		newSoapClient(transaction_url, headers, opts...),
 	)
 	attachmentService := attachment24.NewAttachmentServiceSoap(
-		newSoapClient(attachment_url, headers),
+		newSoapClient(attachment_url, headers, opts...),
 	)
 	payrollService, err := payroll24.New(creds.PayrollAPI)
 	if err != nil {
@@ -140,16 +145,24 @@ func NewClient(creds *Credentials) (*Client, error) {
 	return &client, nil
 }
 
-func newSoapClient(url string, headers map[string]string) *soap.Client {
-	// add headers to auth client for
-	// later we add session id to make all calls authenticated
-	withHeaders := soap.WithHTTPHeaders(headers)
+func NewClient(creds *Credentials) (*Client, error) {
+	return NewClientWithOptions(creds, nil)
+}
 
-	// handle timeouts
-	withTLSHandshakeTimeout := soap.WithTLSHandshakeTimeout(0)
-	withRequestTimeout := soap.WithRequestTimeout(0)
+func newSoapClient(url string, headers map[string]string, opts ...soap.Option) *soap.Client {
+	options := []soap.Option{
+		// add headers to auth client for
+		// later we add session id to make all calls authenticated
+		soap.WithHTTPHeaders(headers),
 
-	return soap.NewClient(url, withHeaders, withTLSHandshakeTimeout, withRequestTimeout)
+		// handle timeouts
+		soap.WithTLSHandshakeTimeout(0),
+		soap.WithTLSHandshakeTimeout(0),
+	}
+
+	options = append(options, opts...)
+
+	return soap.NewClient(url, options...)
 }
 
 func (c *Client) CheckAuth(callback func(sessionId string) error) error {
