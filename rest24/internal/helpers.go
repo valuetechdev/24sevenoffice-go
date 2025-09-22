f11c9d313c902eae1909610edfa4ac7bd2c470c5
Nils Norman Haukås <mail@nilsnh.no>
2025-09-22 15:12:19 +0200

    rest24: create product e2e test (#19)
    * write test for creating product
    
    * remove redundant code

--- rest24/internal/helpers.go
+++ rest24/internal/helpers.go
@@ -1,6 +1,12 @@
 package internal
 
-func SafeDeref[T comparable](v *T) T {
+import (
+	"encoding/json"
+	"math/rand"
+)
+
+// D safely dereferences a value
+func D[T comparable](v *T) T {
 	var zero T
 	if v == nil {
 		return zero
@@ -8,10 +14,30 @@ func SafeDeref[T comparable](v *T) T {
 	return *v
 }
 
-func Ref[T comparable](v T) *T {
+// R returns a pointer value to the type
+func R[T comparable](v T) *T {
 	var zero T
 	if v == zero {
 		return nil
 	}
 	return &v
+}
+
+// NOTE: Only use this for debugging never in production code.
+func DebugJSON(a any) string {
+	j, err := json.MarshalIndent(a, "", "  ")
+	if err != nil {
+		return err.Error()
+	}
+	return string(j)
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
